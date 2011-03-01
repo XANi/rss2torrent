13ca21be9fcd495f81b3d2e2eb9aeb5942624072
Mariusz Gronczewski <xani666@gmail.com>
2011-03-01 19:15:42 +0100

    Initial commit

---
+++ bin/rss2torrent.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use XML::RSSLite;
+use LWP::Simple;
+use Data::Dumper;
+use HTML::Entities;
+my $url=$ARGV[0];
+my $outdir=$ARGV[1];
+my $count=$ARGV[2];
+print "Downloading $count torrents into $outdir from $url\n";
+my $rss=\%rss;
+my $data = get($url);
+parseRSS($rss, \$data);
+my $items = $rss->{'item'};
+foreach my $item (@$items) {
+    my $filename = sanitize($item->{'title'}) . '.torrent';
+    my $torrent_url = HTML::Entities::decode($item->{'link'});
+    print "Downloading $torrent_url into $filename\n";
+    getstore($torrent_url, $outdir . '/' . $filename);
+    if($count <= 1 ) {exit;}
+    --$count;
+    print "\n";
+}
+
+
+#remove spaces, backslashes and other crap
+sub sanitize() {
+    $_ = HTML::Entities::decode(shift);
+    s/ /_/g;
+    s/\\//g;
+    return $_;
+}
